81cd0da3b9964f6c29c41973ecd82baa121749e9
Owen Synge <osynge@suse.com>
2015-04-08 12:03:33 +0200

    Enable systemd in ceph-deploy
    If distrobution uses systemd suport it and enable the deamons on boot.
    
    Signed-off-by: Owen Synge <osynge@suse.com>

--- ceph_deploy/hosts/suse/mon/create.py
+++ ceph_deploy/hosts/suse/mon/create.py
@@ -1,19 +1,52 @@
 from ceph_deploy.hosts import common
 from ceph_deploy.lib import remoto
+import logging
+
+LOG = logging.getLogger(__name__)
 
 
 def create(distro, args, monitor_keyring):
     hostname = distro.conn.remote_module.shortname()
     common.mon_create(distro, args, monitor_keyring, hostname)
-
-    remoto.process.run(
-        distro.conn,
-        [
-            'rcceph',
-            '-c',
-            '/etc/ceph/{cluster}.conf'.format(cluster=args.cluster),
-            'start',
-            'mon.{hostname}'.format(hostname=hostname)
-        ],
-        timeout=7,
-    )
+    if distro.init == 'systemd':  # Ubuntu uses upstart
+        remoto.process.run(
+            distro.conn,
+            [
+                'systemctl',
+                'enable',
+                'ceph-mon@{hostname}'.format(hostname=hostname)
+            ],
+            timeout=7,
+        )
+        remoto.process.run(
+            distro.conn,
+            [
+                'systemctl',
+                'start',
+                'ceph-mon@{hostname}'.format(hostname=hostname)
+            ],
+            timeout=7,
+        )
+        remoto.process.run(
+            distro.conn,
+            [
+                'systemctl',
+                'enable',
+                'ceph.target'
+            ],
+            timeout=7,
+        )
+    elif distro.init == 'sysvinit':  # Debian uses sysvinit
+        remoto.process.run(
+            distro.conn,
+            [
+                '/etc/init.d/ceph',
+                '-c',
+                '/etc/ceph/{cluster}.conf'.format(cluster=args.cluster),
+                'start',
+                'mon.{hostname}'.format(hostname=hostname)
+            ],
+            timeout=7,
+        )
+    else:
+        raise RuntimeError('create cannot use init %s' % distro.init)
